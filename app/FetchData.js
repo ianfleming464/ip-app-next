fe7f9567e35965afdef12ad5b7a795abc9e30ac5
Ian Fleming <ianflemingdeveloper@gmail.com>
2024-05-31 20:29:28 +0200

    Initial commit, env

---
+++ app/FetchData.js
@@ -0,0 +1,18 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.PROJECT_URL;
+const supabaseAnonKey = process.env.SERVICE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export async function TrademarkInfo() {
+  let { data, error } = await supabase.from('trademark-info').select('*');
+  if (error) console.error('Error: ', error);
+  else console.log('Trademarks: ', data);
+}
+
+export async function IndustrialDesignInfo() {
+  let { data, error } = await supabase.from('industrial-design-info').select('*');
+  if (error) console.error('Error: ', error);
+  else console.log('Industrial Designs: ', data);
+}
